dc1be1e8c608c6e53543e3f9b1d9c3b8556c2cee
salimchoura <salim.choura@yahoo.com>
2023-12-01 00:10:06 -0700

    added bar chart for reviews and comments

--- public_html/recipe.js
+++ public_html/recipe.js
@@ -104,7 +104,7 @@ const widget = document.querySelector(".star-widget");
 const editBtn = document.querySelector(".edit");
 
 
-
+showReviews()
 
 
 /**
@@ -158,19 +158,17 @@ function showReviews() {
         return response.text();
     }).then((text) => {
         if (text == 'COULD NOT FIND REVIEWS') {
-            document.getElementById('recipeBlock').innerHTML += '<p>Could not load reviews.</p>';
+            document.getElementById('recipe').innerHTML += '<p>Could not load reviews.</p>';
         }
         else {
             let reviews = JSON.parse(text);
+            document.getElementById('recipe').innerHTML += '<h2>Reviews Statistics</h2>'
+            makeBarChart(reviews)
+            document.getElementById('comments').innerHTML = ''
             // put the reviews into HTML
             for (let review of reviews) {
                 // add each review to the DOM
-                document.getElementById('recipeBlock').innerHTML += '<h3>' + review.user + '<h3>';
-                let numStars = review.stars;
-                // add each star for the user
-                for (let i = 0; i < numStars; i++) {
-                    document.getElementById('recipeBlock').innerHTML += "<img class='reviewStars' src='star.png'>";
-                }
+                document.getElementById('comments').innerHTML += `<div class='comment'><h3> ${review.user} </h3><p> ${review.text} </p></div>`;
             }
         }
     })
@@ -187,4 +185,74 @@ for (let i = 0; i < inputs.length; i++) {
 
 btn.onclick = () => {
     addReview(stars)
+}
+
+
+function makeBarChart(reviews) {
+    let formatted = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
+    for (let review of reviews) {
+        formatted[review.stars] += 1
+    }
+
+    let formattedAgain = []
+    for (let i = 1; i < 6; i++) {
+        let obj = { stars: i, count: formatted[i] }
+        formattedAgain.push(obj)
+    }
+
+    console.log(formattedAgain)
+
+    const width = 600;
+    const height = 400;
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+    // Calculate the inner dimensions
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+
+    const svg = d3.select("#recipe")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+    const chart = svg.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Create x and y scales
+    const xScale = d3.scaleBand()
+        .domain(formattedAgain.map(d => d.stars))
+        .range([0, innerWidth])
+        .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(formattedAgain, d => d.count)])
+        .range([innerHeight, 0]);
+
+    // Create x and y axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    // Append x and y axes to the chart
+    chart.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${innerHeight})`)
+        .call(xAxis);
+
+    chart.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
+
+    // Create bars
+    chart.selectAll(".bar")
+        .data(formattedAgain)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => xScale(d.stars))
+        .attr("y", d => yScale(d.count))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => innerHeight - yScale(d.count));
+
+
 }
